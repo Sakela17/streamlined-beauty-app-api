762f373379a38cd685e0be22c1824bf523dc0f87
Marina Valiquette <marina.valiquette@gmail.com>
2018-05-31 21:43:03 -0400

    Add GET by id endpoint

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,11 +3,31 @@
 const router = require('express').Router();
 const knex = require('../knex');
 
+/* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
   knex('profiles')
     .select('id', 'full_name', 'email', 'password', 'location', 'role')
     .then(results => {
       res.json(results);
+    })
+    .catch(next);
+});
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/:id', (req, res, next) => {
+  const userId = req.params.id;
+
+
+  knex
+    .first('id', 'full_name', 'email', 'password', 'location', 'role')
+    .from('profiles')
+    .where('id', userId)
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        next();
+      }
     })
     .catch(next);
 });
